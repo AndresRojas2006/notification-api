6baafa0aa3a29718cc91af8def7609a0071ffec2
AndresRojas <andres2007benavides@gmail.com>
2025-06-04 18:15:11 -0500

    Backend funcionando - listito para Railway

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsModule } from './notifications/notifications.module';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', 
+      password: '123456',  
+      database: 'notification_db', 
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
+})
+export class AppModule {}
